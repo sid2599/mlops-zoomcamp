620ec32f4983722c74edd9fd19a78aef28001d7f
sid2599 <siddharthjp1999@gmail.com>
2024-05-29 22:00:42 +0000

    contin-he

--- week2/homework/train.py
+++ week2/homework/train.py
@@ -1,9 +1,15 @@
 import os
 import pickle
 import click
+import mlflow
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
+
+
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_experiment("taxi-prediction")
+mlflow.autolog()
 
 
 def load_pickle(filename: str):
@@ -21,13 +27,20 @@ def run_train(data_path: str):
 
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+    
+    
+    with mlflow.start_run():    
+        #min_samples_split = 2
+        #mlflow.log_param("min_samples_split",min_samples_split)
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
 
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
 
 
 if __name__ == '__main__':
+    
+    
     run_train()
